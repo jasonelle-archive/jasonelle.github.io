c38481183f5836bdc6d7f9ddd5f50d650a8f1003
Camilo <camilo@ninjas.cl>
2019-03-23 19:56:27 -0300

    improved travis

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run --rm -v $PWD:/source yanqd0/hugo /bin/sh -c 'cd /source/src && hugo && mv /source/src/public /source/site'
+docker run --rm -v $PWD:/source yanqd0/hugo /bin/sh -c 'cd /source/src && hugo && mv /source/src/public /source/site && chmod 777 -Rf /source/site'
