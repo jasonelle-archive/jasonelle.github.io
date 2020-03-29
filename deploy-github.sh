d90be0fb6d3b829a8aa019398d5e6bab3511654a
Camilo Castro <camilo@ninjas.cl>
2020-03-29 14:58:36 -0300

    improved travis

--- deploy-github.sh
+++ deploy-github.sh
@@ -6,18 +6,22 @@ set -e
 rm -rf src
 rm -rf .git
 
+# get the master branch to the web dir
 git clone -b master https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git web
-
 cd web
 
-#                                                         continue exec if error
+# remove git repo
+# continue exec if error
 find . -not -path "./.git*" -not -name "." | xargs rm -rf || true
 
+# copy files to the current dir
 cp -rf ../site/* .
 
+# removing not used folder
 rm -rf ../site
 
-git add --all *
+# add changes to the commit
+git add .
 git commit -m "deployed docs (travis build ${TRAVIS_BUILD_NUMBER})"
 git push -q -f origin master
 echo "Deployed"
